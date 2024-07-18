bcdd010d2df4f7c816777baf9a908f941a69b088
afaqez <afaqkhan113@gmail.com>
2024-07-18 08:01:27 +0500

    Remove TextField Component from Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { useTheme } from "@mui/material/styles";
 import logo from "../assets/images/logo.svg";
